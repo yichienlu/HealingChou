6d13903d53d60784cb1ff20846058945f8c2b42b
yichienlu <yichien.lu89@gmail.com>
2023-03-16 11:54:16 +0800

    deploy

---
+++ assets/AdminServices-4deaeb21.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-a6ecb31c.js";const t={};function r(s,n,a,_,p,i){return c(),o("div")}const f=e(t,[["render",r]]);export{f as default};
---
+++ assets/AppointmentSuccessView-31167c6d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-a6ecb31c.js";const o={};function s(n,r,a,p,_,i){return t(),c("div")}const d=e(o,[["render",s]]);export{d as default};
