9a6ffb1fc432b65a3b76cc6b52abef7726381e0f
yichienlu <yichien.lu89@gmail.com>
2023-03-29 16:47:29 +0800

    feat: 首頁樣式

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -44,7 +44,7 @@
         學會認識自己、更愛自己以及療癒自己。
       </p>
     </section>
-    <section class="bg-secondary py-4">
+    <section class="bg-primary py-4">
       <h3 class="container px-3 text-center text-white">
         <div>想獲得更多希塔療癒、塔羅占卜的訊息？</div>
         <div>來與舟舟聊療吧！</div>
